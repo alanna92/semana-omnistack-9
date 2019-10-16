e72c4fbbf36b59ca4b02263cdd46a7f93aa140a1
Alanna Vanzella <alanna92@gmail.com>
2019-10-15 21:52:29 -0300

    Finalizando backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,16 +4,41 @@ const routes = require('./routes');
 const cors = require('cors');
 const path = require('path');
 
+//ouvir socket
+const socketio = require('socket.io');
+const http = require('http');
+
 const app = express();
+const server = http.Server(app);
+const io = socketio(server); 
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-6axal.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    console.log(socket.handshake.query);
+
+    const { user_id } = socket.handshake.query;
+    console.log('Usuario conectado', socket.id, user_id);
+
+    connectedUsers[user_id] = socket.id;
+})
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
+
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
